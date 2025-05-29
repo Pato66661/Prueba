574bdba649998a482666be261717479c56d3cd86
Patricio Estevez <pato66661@hotmail.com>
2025-05-28 22:54:16 -0500

    inferir.py

--- inferir.py
+++ inferir.py
@@ -3,7 +3,7 @@ import joblib
 # Datos fijos para prueba
 features = [[5.1, 3.5, 1.4, 0.2]]
 # Cargar el modelo
-modelo = joblib.load("model.pkl")
+modelo = joblib.load("modelo_random_forest.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])
